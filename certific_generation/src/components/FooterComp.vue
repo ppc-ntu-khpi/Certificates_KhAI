97280ce81c806435d6edbf57c11f6e7d4ccb6149
Alexander Babich <alexander.taurus@gmail.com>
2025-05-06 08:20:35 +0000

    minor changes

--- certific_generation/src/components/FooterComp.vue
+++ certific_generation/src/components/FooterComp.vue
@@ -2,7 +2,7 @@
   <footer class="py-3 bg-dark fixed-bottom">
     <div class="container">
       <p class="m-0 text-center text-white">
-        Copyright &copy; Anureva Katerina 2024
+        Copyright &copy; Angelina Babych 2025
       </p>
     </div>
   </footer>
